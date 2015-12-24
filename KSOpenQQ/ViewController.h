8ed653f20acf9a5450feb9e7f6a390243e638f0b
xiaoshi <sjl_leaf@163.com>
2015-12-24 12:50:53 +0800

    Initial Commit

---
+++ KSOpenQQ/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KSOpenQQ
+//
+//  Created by 史金亮 on 15/12/24.
+//  Copyright © 2015年 kamy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
