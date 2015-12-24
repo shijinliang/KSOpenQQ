b102f8745c3b5eb407d151d3cbbc7488eaf1c445
xiaoshi <sjl_leaf@163.com>
2015-12-24 13:01:24 +0800

    first commit

--- KSOpenQQ/ViewController.m
+++ KSOpenQQ/ViewController.m
@@ -8,8 +8,10 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UIWebViewDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *qqNumText;
 
+@property (nonatomic, strong) UIWebView *webView;
 @end
 
 @implementation ViewController
@@ -17,11 +19,25 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.webView = [[UIWebView alloc] initWithFrame:CGRectZero];
+    [self.view addSubview:self.webView];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)openQQChat:(UIButton *)sender {
+    if (self.qqNumText.text.length == 0) {
+        return;
+    }
+    //拼写打开QQ 的URL
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"mqq://im/chat?chat_type=wpa&uin=%@&version=1&src_type=web",self.qqNumText.text]];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    self.webView.delegate = self;
+    [self.webView loadRequest:request];
+    
+    [self.view endEditing:YES];
 }
 
 @end
